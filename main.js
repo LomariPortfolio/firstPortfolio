baf931887945adc91182a7280d26174da2c86957
LomariPortfolio <lomari.hamza.it@gmail.com>
2023-07-28 16:57:05 +0100

    :fire: added some nice animation in the homepage

---
+++ main.js
@@ -0,0 +1,10 @@
+let txt = '//You can also use the buttons above to move in the website.';
+let speed = 60;
+let i = 0;
+function haxxorEffect() {
+    if (i < txt.length) {
+    document.getElementById("haxxorAnim").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(haxxorEffect, speed);
+  }
+}
